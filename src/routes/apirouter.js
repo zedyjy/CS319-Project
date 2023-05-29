3c43b49f3b58747dc1f3326666df49f795fda15c
yagizalkilic <yagiz.alkilic1@gmail.com>
2023-05-30 02:45:22 +0300

    small fixes

--- src/routes/apirouter.js
+++ src/routes/apirouter.js
@@ -25,7 +25,7 @@ const { error } = require("jquery");
 
 // ------- mongo db connection --------
 mongoose.connect("mongodb://127.0.0.1:27017/internship-system");
-const database = mongoose.connection; //get the databa object from mongoose connection
+const database = mongoose.connection; //get the database object from mongoose connection
 
 database.on("error", (error) => {
   console.log(error);
